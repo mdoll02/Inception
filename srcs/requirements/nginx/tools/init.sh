4c52b387eab9d252969a23ef1b03ba87a0cb497d
mdoll <mdoll@student.42wolfsburg.de>
2023-10-05 11:04:33 +0200

    Dockerfile and init script for nginx

---
+++ srcs/requirements/nginx/tools/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout /etc/ssl/private/nginx.key \
+  -out /etc/ssl/certs/nginx.crt \
+  -subj $SUBJ
+
+envsubset '$DOMAIN_NAME' < /nginx.conf > /etc/nginx/conf.d/default.conf
+
+nginx -g 'daemon off;'
