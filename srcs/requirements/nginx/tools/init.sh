2f2a292741875ba27f8e6270fe00a1a10129e9a6
mdoll <mdoll@student.42wolfsburg.de>
2023-10-10 12:53:02 +0200

    removed unnecessary command

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -keyout /etc/ssl/private/nginx.key \
   -out /etc/ssl/certs/nginx.crt \
   -subj $SUBJ
-
-envsubst '$DOMAIN_NAME' < /nginx.conf > /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
