79d1862d2ed98555867e04e5b921897e984a2eac
mdoll <mdoll@student.42wolfsburg.de>
2023-10-13 13:02:28 +0200

    removed init.sql to use env variables

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mysql --user=mysql < /dbinit.sql
+
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$ROOT_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mysql < db1.sql
