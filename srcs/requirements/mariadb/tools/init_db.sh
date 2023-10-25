fa80a6b14123068797ee5f733eff4804070f3a92
mdoll <mdoll@student.42wolfsburg.de>
2023-10-25 13:54:20 +0200

    fixed mariadb installation

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,16 +1,40 @@
-#!/bin/bash
+#!/bin/sh
 
-service mysql start 
+mysql_install_db
 
+/etc/init.d/mysql start
 
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWORD' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+if [ -d "/var/lib/mysql/$DB_NAME" ]
+then 
 
-mysql -uroot < db1.sql
+	echo "Database already exists"
+else
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+# enforce root password
 
-mysqld
+mysql_secure_installation <<_EOF_
+
+Y
+1234
+1234
+Y
+n
+Y
+Y
+_EOF_
+
+#Add a root user on lcalhost
+
+	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+
+#Create database and user for wordpress
+	echo "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+
+#Import database
+mysql -uroot -p$ROOT_PASSWORD $DB_NAME < /usr/local/bin/wordpress.sql
+
+fi
+
+/etc/init.d/mysql stop
+
+exec "$@"
