2ecbeb7db324a473d7ff372a339a6139e209afa3
mdoll <mdoll@student.42wolfsburg.de>
2023-11-07 15:30:00 +0100

    updated mariadb script

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-# Initialize the MariaDB server
-mysql_install_db --user=mysql --ldata=/var/lib/mysql || { echo "MariaDB initialization failed"; exit 1; }
+mkdir -p /var/run/mysqld
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+chmod -R 777 /var/run/mysqld /var/lib/mysql
 
 # Start the MariaDB service
-service mysql start || { echo "MariaDB service start failed"; exit 1; }
-
-# Create a temporary SQL file to set the root password
-cat <<EOF > /tmp/sqlfile.sql
-UPDATE mysql.user SET Password=PASSWORD('$ROOT_PASSWORD') WHERE User='root';
-FLUSH PRIVILEGES;
-EOF
-
-# Run SQL file to set the root password
-mysql < /tmp/sqlfile.sql || { echo "Root password change failed"; exit 1; }
-
-# Remove the temporary SQL file
-rm -f /tmp/sqlfile.sql
+service mysql start
 
 # Create the database
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root || { echo "Database creation failed"; exit 1; }
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > init.sql
 
 # Create the user and grant privileges
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql -u root || { echo "User creation failed"; exit 1; }
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" | mysql -u root || { echo "Privilege grant failed"; exit 1; }
-echo "FLUSH PRIVILEGES;" | mysql -u root || { echo "Flush privileges failed"; exit 1; }
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> init.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
+
+mysql < init.sql
 
 # Stop the MariaDB service
-service mysql stop || { echo "MariaDB service stop failed"; exit 1; }
+service mysql stop
 
 # Start MariaDB without binding to all interfaces
 mysqld
